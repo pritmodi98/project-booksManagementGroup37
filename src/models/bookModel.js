2a18fe2d98f9a568a53d099626c4b0089489d274
Prit_Modi <pritmodi98@gmail.com>
2022-05-09 16:47:14 +0530

    start of project3

---
+++ src/models/bookModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// const collegeSchema = new mongoose.Schema( {
+//     name: {
+//         type:String,
+//         unique:true,
+//         required:true
+//     },
+//     fullName:{
+//         type:String,
+//         required:true
+//     },
+//     logoLink: {
+//         type: String,
+//         // validate: {
+//         //     validator: value => validator.isURL(value, { protocols: ['http', 'https', 'ftp'], require_tld: true, require_protocol: true }),
+//         //     message: 'Must be a Valid URL'
+//         // },
+//         required:true
+//     },
+//     isDeleted:{
+//         type:Boolean,
+//         default:false
+//     }
+    
+// });
+
+module.exports = mongoose.model('newCollege', collegeSchema)
