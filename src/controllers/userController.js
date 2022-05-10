f53c0ebb2131c03d602e5a20356e2eb00b6a095f
Richa5270 <102970121+Richa5270@users.noreply.github.com>
2022-05-10 13:41:17 +0530

    login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,22 +6,22 @@ const createUser = async function (req, res) {
     if (!data) {
       return res
         .status(400)
-        .send({ status: false, massege: "plz enter a valid data" });
+        .send({ status: false, message: "plz enter a valid data" });
     }
     if (["Mr", "Mrs", "Miss"].indexOf(data.title) == -1) {
       return res.status(400).send({
         status: false,
-        data: "Enter a valid title Mr or Mrs or Miss ",
+        message: "Enter a valid title Mr or Mrs or Miss ",
       });
     }
     if (!data.name) {
       return res
          .status(400)
-         .send({ status: false, data: "Name is required" });
+         .send({ status: false, message: "Name is required" });
     }
     //console.log(data.name)
     if (!/^([a-zA-Z]+)$/.test(data.name)) {
-        return res.status(400).send({ status: false, massege: "plz enter name" });
+        return res.status(400).send({ status: false, message: "plz enter name" });
       }
     if (!data.phone) {
         return res
@@ -37,53 +37,53 @@ const createUser = async function (req, res) {
       }
       let checkphone = await userModel.find({phone:data.phone});
       if (checkphone.length !=0){
-          return res.status(409).send({status:false, massege:"phone no already exsit"})
+          return res.status(409).send({status:false,message:"phone no. already exsit"})
       }
     if (!data.email) {
       return res
         .status(400)
-        .send({ status: false, data: "EmailId is required" });
+        .send({ status: false, message: "EmailId is required" });
     }
     if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(data.email)) {
       return res
         .status(400)
-        .send({ status: false, data: "plz enter a valid Email" });
+        .send({ status: false, message: "plz enter a valid Email" });
     }
     let checkIfemailExist = await userModel.find({ email: data.email });
     if (checkIfemailExist.length != 0) {
       return res
         .status(409)
-        .send({ status: false, data: "email already exist" });
+        .send({ status: false, message: "email already exist" });
      }
     if (!data.password) {
       return res
         .status(400)
-        .send({ status: false, data: "password is required" });
+        .send({ status: false, message: "password is required" });
     }
     if(!/^([a-zA-Z0-9!@#$%^&*_\-+=><]{8,16})$/.test(data.password.trim())){
-        return res.status(400).send({status: false, massege: "Plz enter valid Password"})
+        return res.status(400).send({status: false, message: "Plz enter valid Password"})
     }
     let address = data.address;
     if(!address){
-        return res.status(400).send({status:false, massege:"plz enter address"})
+        return res.status(400).send({status:false, message:"plz enter address"})
     }
     if(Object.keys(address).length < 3){
-        return res.status(400).send({status:false, massege:"address is required"})
+        return res.status(400).send({status:false, message:"address is required"})
     }
     if (!/^([a-zA-Z0-9#, ]+)$/.test(address.street)) {
-        return res.status(400).send({ status: false, massege: "plz enter valid street" });
+        return res.status(400).send({ status: false, message: "plz enter valid street" });
       }
     if (!/^([a-zA-Z]+)$/.test(address.city)) {
-        return res.status(400).send({ status: false, massege: "plz enter valid city" });
+        return res.status(400).send({ status: false, message: "plz enter valid city" });
       }
     if (!/^([0-9]{6})$/.test(address.pincode)) {
-        return res.status(400).send({ status: false, massege: "plz enter valid pincode" });
+        return res.status(400).send({ status: false, message: "plz enter valid pincode" });
       }
      let saved = await userModel.create(data);
-    res.status(201).send({ status: true,massege:'Success', data: saved });
+    res.status(201).send({ status: true,message:'Success', data: saved });
   } catch (err) {
     console.log("This is the err", err.message);
-    res.status(500).send({ status: false, massege: err.message });
+    res.status(500).send({ status: false, message: err.message });
   }
 };
 
