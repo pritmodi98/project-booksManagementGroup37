fe59e5a343e85b0215f6ab71359f9e78e0db1546
Prit_Modi <pritmodi98@gmail.com>
2022-05-10 11:26:10 +0530

    ddw

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,15 @@ const userModel = require("../Models/userModel")
 const createUser = async function (req, res) {
   try {
     let data = req.body;
-    if (!data) {
+    if (Object.keys(data).length===0 || typeof data==='undefined' || typeof data==='null') {
       return res
         .status(400)
         .send({ status: false, massege: "plz enter a valid data" });
+    }
+    if (!data.title) {
+        return res
+         .status(400)
+         .send({ status: false, data: "title is required" });
     }
     if (["Mr", "Mrs", "Miss"].indexOf(data.title) == -1) {
       return res.status(400).send({
@@ -21,34 +26,29 @@ const createUser = async function (req, res) {
     }
     //console.log(data.name)
     if (!/^([a-zA-Z]+)$/.test(data.name)) {
-        return res.status(400).send({ status: false, massege: "plz enter name" });
+        return res.status(400).send({ status: false, massege: "special characters are not allowed in name" });
       }
-    if (!data.phone) {
+      if (!data.email) {
+        return res
+          .status(400)
+          .send({ status: false, data: "EmailId is required" });
+      }
+     if (!data.phone) {
         return res
           .status(400)
           .send({ status: false, message: "phone No. is required" });
       }
-      if (
-        !/^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/.test(data.phone)
-      ) {
+      if (!/^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/.test(data.phone)) {
         return res
           .status(400)
-          .send({ status: false, message: "Phone No is required" });
+          .send({ status: false, message: "enter 10 digit phone number" });
       }
       let checkphone = await userModel.find({phone:data.phone});
       if (checkphone.length !=0){
           return res.status(409).send({status:false, massege:"phone no already exsit"})
       }
-    if (!data.email) {
-      return res
-        .status(400)
-        .send({ status: false, data: "EmailId is required" });
-    }
-    if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(data.email)) {
-      return res
-        .status(400)
-        .send({ status: false, data: "plz enter a valid Email" });
-    }
+    
+    
     let checkIfemailExist = await userModel.find({ email: data.email });
     if (checkIfemailExist.length != 0) {
       return res
@@ -79,7 +79,7 @@ const createUser = async function (req, res) {
     if (!/^([0-9]{6})$/.test(address.pincode)) {
         return res.status(400).send({ status: false, massege: "plz enter valid pincode" });
       }
-     let saved = await userModel.create(data);
+    let saved = await userModel.create(data);
     res.status(201).send({ status: true, data: saved });
   } catch (err) {
     console.log("This is the err", err.message);
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const userController=require("../controllers/userController")
 
 router.post('/register',userController.createUser)
-=======
-const userController = require("../controllers/userController")
-
-router.post("/register",userController.createUser)
->>>>>>> b57feecdf395b9c696184b1ef1964bd626b6a9b1
 
 module.exports=router;
 
